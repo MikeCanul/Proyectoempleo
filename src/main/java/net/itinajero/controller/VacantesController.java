e2676a78edd879272d65b40fe3ef2f5373e5b383
MikeCanul <mike.canuljimenez@cbtis72.edu.mx>
2022-10-10 23:18:58 -0500

    Commit 6
    Commit 6-video18

--- src/main/java/net/itinajero/controller/VacantesController.java
+++ src/main/java/net/itinajero/controller/VacantesController.java
@@ -9,6 +9,8 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.beans.propertyeditors.CustomDateEditor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
@@ -56,6 +58,16 @@ public class VacantesController {
     	model.addAttribute("vacantes", lista);
 		return "vacantes/listVacantes";
 	}
+	
+	
+	
+	@GetMapping(value = "/indexPaginate")
+	public String mostrarIndexPaginado(Model model, Pageable page) {
+	Page<Vacante> lista = serviceVacantes.buscarTodas(page);
+	model.addAttribute("vacantes", lista);
+	return "vacantes/listVacantes";
+	}
+
 	
 	
 	
--- src/main/java/net/itinajero/service/IVacantesService.java
+++ src/main/java/net/itinajero/service/IVacantesService.java
@@ -2,6 +2,10 @@ package net.itinajero.service;
 
 import java.util.List;
 
+import org.springframework.data.domain.Example;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+
 import net.itinajero.model.Vacante;
 
 
@@ -11,4 +15,7 @@ public interface IVacantesService {
 	void guardar(Vacante vacante);
 	List<Vacante> buscarDestacadas();
 	void eliminar(Integer idVacante);
+	List<Vacante>buscarByExample(Example<Vacante> example);
+	Page<Vacante> buscarTodas(Pageable page);
+	
 }
