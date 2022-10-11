2ab95626364c4ba1a5dcddc598105de0cbb4085d
MikeCanul <mike.canuljimenez@cbtis72.edu.mx>
2022-10-10 21:05:45 -0500

    Commit 1-video3

---
+++ src/main/java/net/itinajero/service/db/CategoriasServiceJpa.java
@@ -0,0 +1,43 @@
+package net.itinajero.service.db;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import net.itinajero.model.Categoria;
+import net.itinajero.repository.CategoriasRepository;
+import net.itinajero.service.ICategoriasService;
+
+
+@Service
+public class CategoriasServiceJpa implements ICategoriasService {
+
+	@Autowired
+	private CategoriasRepository categoriasRepo;
+	
+	@Override
+	public void guardar(Categoria categoria) {
+		// TODO Auto-generated method stub
+		categoriasRepo.save(categoria);
+
+	}
+
+	@Override
+	public List<Categoria> buscarTodas() {
+		// TODO Auto-generated method stub
+		return categoriasRepo.findAll();
+	}
+
+	@Override
+	public Categoria buscarPorId(Integer idCategoria) {
+		// TODO Auto-generated method stub
+		Optional<Categoria> optional = categoriasRepo.findById(idCategoria);
+		if(optional.isPresent()) {
+			return optional.get();
+		}
+		return null;
+	}
+
+}
