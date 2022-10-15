bb50c6856616e3333fa9bb1a19a8aac3deef640c
MikeCanul <mike.canuljimenez@cbtis72.edu.mx>
2022-10-15 11:34:14 -0500

    Commit 10
    Commit 10 -video 3 Carpeta 14

--- src/main/java/net/itinajero/controller/CategoriasController.java
+++ src/main/java/net/itinajero/controller/CategoriasController.java
@@ -86,7 +86,7 @@ public class CategoriasController {
 		return "redirect:/categorias/index";
 		
 	}
-	
+		
 	
 	
 	
--- src/main/java/net/itinajero/controller/homeController.java
+++ src/main/java/net/itinajero/controller/homeController.java
@@ -8,6 +8,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.propertyeditors.StringTrimmerEditor;
 import org.springframework.data.domain.Example;
 import org.springframework.data.domain.ExampleMatcher;
+import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.WebDataBinder;
@@ -38,8 +39,12 @@ public class HomeController {
 	@Autowired
    	private IUsuariosService serviceUsuarios;
 	
-	
-	
+	@GetMapping("/index")
+	public String mostrarIndex(Authentication auth) {
+		String username = auth.getName();
+		System.out.println("Nombre del usuario: " + username);
+		return "redirect:/";
+	}
 	
 	
 	@GetMapping("/signup")
