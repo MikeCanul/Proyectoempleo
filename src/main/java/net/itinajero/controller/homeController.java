0b19542c7dbc8f64b482cc599a120b5764dd4eff
MikeCanul <mike.canuljimenez@cbtis72.edu.mx>
2022-10-17 14:29:31 -0500

    Commi 13
    Commit 13- video 12 carpeta 14

--- src/main/java/net/itinajero/controller/homeController.java
+++ src/main/java/net/itinajero/controller/homeController.java
@@ -12,13 +12,16 @@ import org.springframework.data.domain.Example;
 import org.springframework.data.domain.ExampleMatcher;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.WebDataBinder;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.InitBinder;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import net.itinajero.model.Perfil;
@@ -41,6 +44,9 @@ public class HomeController {
 	
 	@Autowired
    	private IUsuariosService serviceUsuarios;
+	
+	@Autowired
+	private PasswordEncoder passwordEncoder;
 	
 	@GetMapping("/index")
 	public String mostrarIndex(Authentication auth, HttpSession session) {
@@ -69,6 +75,11 @@ public class HomeController {
 	
 	@PostMapping("/signup")
 	public String guardarRegistro(Usuario usuario, RedirectAttributes attributes) {
+		
+		String pwdPlano = usuario.getPassword();
+		String  pwdEncriptado = passwordEncoder.encode(pwdPlano);
+		usuario.setPassword(pwdEncriptado);
+		
 		usuario.setEstatus(1); // Activado por defecto
 		usuario.setFechaRegistro(new Date()); // Fecha de Registro, la fecha actual del servidor
 		
@@ -122,6 +133,12 @@ public class HomeController {
 		model.addAttribute("empleos", lista);
 		
 		return "listado";
+	}
+	
+	@GetMapping("/bcrypt/{texto}")
+	@ResponseBody
+	public String encriptar(@PathVariable("texto") String texto) {
+		return texto + " Encriptado en Bcrypt: " + passwordEncoder.encode(texto);
 	}
 	
 	
