906dffeef7da147e57f5b417b6e9a67ab2f8c145
MikeCanul <mike.canuljimenez@cbtis72.edu.mx>
2022-10-17 14:30:43 -0500

    Commit 14
    Commit 14 -video 15 carpeta 14

--- src/main/java/net/itinajero/security/DatabaseWebSecurity.java
+++ src/main/java/net/itinajero/security/DatabaseWebSecurity.java
@@ -54,7 +54,7 @@ public class DatabaseWebSecurity extends WebSecurityConfigurerAdapter {
 		// Todas las demás URLs de la Aplicación requieren autenticación
 		.anyRequest().authenticated()
 		// El formulario de Login no requiere autenticacion
-		.and().formLogin().permitAll();
+		.and().formLogin().loginPage("/login").permitAll();
 		}
 	
 		@Bean
